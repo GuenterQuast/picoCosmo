d883940a7d0df379e5ebc73672d32873db741d17
Guenter Quast <guenter.quast@online.de>
2026-06-23 18:20:40 +0200

    latest PicoScope libraries, arm64 support

--- installlibs_RasPi.sh
+++ installlibs_RasPi.sh
@@ -37,8 +37,27 @@ source "$PHYPYENV"/bin/activate
 python -m pip install --no-build-isolation .
 
 # install PicoScope C libraries for USB oscilloscope
-sudo dpkg -i libs/RasPi/picoscopelibs/*.deb # picoscope 
-sudo usermod -a -G tty pi # grant acces to USB for user pi
+while true; do
+    read -p "Do you wish to install the PicoScope drivers? " yn
+    case $yn in
+        [Yy]* ) echo "Installing PicoScope drivers";
+	  _arch=`uname -m`
+          if [[ "$_arch" == "aarch64" ]]; then
+	      sudo dpkg -i installlibs/picoscope64libs/*.deb; # picoscope for arm64
+	      sudo apt -f install # install all dependencies 	
+              sudo usermod -a -G tty $USER; # grant access to USB for the current user
+          elif [[ "$_arch" == "armhf" ]]; then
+	      sudo dpkg -i installlibs/picoscopelibs/*.deb; # picoscope for amrhf
+	      sudo apt -f install # install all dependencies 	
+              sudo usermod -a -G tty $USER; # grant access to USB for the current user
+	  else
+	      echo "!!! no libraries for architecture: $_arch"
+	  fi
+              break;;
+        [Nn]* ) echo "Skipping PicoScope driver installation"; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 # generate desktop icons
 cp libs/RasPi/*.desktop ~/Desktop
